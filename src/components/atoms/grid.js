cac708735671075c3e2b31675dfb9710a46550cb
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-07-29 13:46:45 -0700

    app up
    app running on 25x25 grid, start/stop button, clear and random button.
    
    need to adjust android settings to allow for zoom.
    
    Need to finish styling.

--- src/components/atoms/grid.js
+++ src/components/atoms/grid.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import {
   Text,
   View,
@@ -6,34 +6,121 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  Button,
 } from 'react-native';
+import produce from 'immer';
+const numRows = 25;
+const numCols = 25;
 
-const numRows = 50;
-const numCols = 50;
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const createEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
 
 const Grid = () => {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
+    return createEmptyGrid();
   });
-  const onPressHandler = () => {
-    console.log('Pressed!')
-  }
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef();
+  runningRef.current = running;
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, 100);
+  }, []);
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <TouchableOpacity
-            key={`${i}-${k}`}
-            style={styles.box}
-            onPress={onPressHandler}
-          />
-        )),
-      )}
-    </ScrollView>
+    <>
+      <Button
+        title={running ? 'stop' : 'start'}
+        onPress={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+      />
+      <Button
+        title="Clear"
+        onPress={() => {
+          setGrid(createEmptyGrid());
+        }}
+      />
+      <Button
+        title="Generate Random"
+        onPress={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0)),
+            );
+          }
+          setGrid(rows);
+        }}
+      />
+      <ScrollView contentContainerStyle={styles.container}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <TouchableOpacity
+              key={`${i}-${k}`}
+              style={{
+                width: 20,
+                height: 20,
+                margin: 1,
+                aspectRatio: 1,
+                backgroundColor: grid[i][k] ? 'powderblue' : 'black',
+              }}
+              onPress={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                console.log('Pressed');
+                setGrid(newGrid);
+              }}
+            />
+          )),
+        )}
+      </ScrollView>
+    </>
   );
 };
 
@@ -46,13 +133,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flexWrap: 'wrap',
     alignItems: 'flex-start',
-    justifyContent: 'space-evenly',
-  },
-  box: {
-    width: 25,
-    height: 25,
-    margin: 1,
-    aspectRatio: 1,
-    backgroundColor: 'powderblue',
+    justifyContent: 'space-between',
+    width: 550,
   },
 });

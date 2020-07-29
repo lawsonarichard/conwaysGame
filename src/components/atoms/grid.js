363da3b44ffc5be0584bbec3657bdb830ee8f740
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-07-29 11:57:27 -0700

    grid rendering
    grid is rendering but cannot scroll need to fix

--- src/components/atoms/grid.js
+++ src/components/atoms/grid.js
@@ -1,8 +1,58 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+
+const numRows = 50;
+const numCols = 50;
 
 const Grid = () => {
-  return <Text>Grid.js</Text>;
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+  const onPressHandler = () => {
+    console.log('Pressed!')
+  }
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <TouchableOpacity
+            key={`${i}-${k}`}
+            style={styles.box}
+            onPress={onPressHandler}
+          />
+        )),
+      )}
+    </ScrollView>
+  );
 };
 
 export default Grid;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+  },
+  box: {
+    width: 25,
+    height: 25,
+    margin: 1,
+    aspectRatio: 1,
+    backgroundColor: 'powderblue',
+  },
+});
